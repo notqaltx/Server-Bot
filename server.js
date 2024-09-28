eb1d03f0b21f9bc61d38370d32ac35c09e09578f
qaltx <67969197+notqaltx@users.noreply.github.com>
2024-09-29 01:40:51 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 
 const { Client, GatewayIntentBits } = require('discord.js');
-const client = new Client({ 
-  intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMembers] 
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMembers,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent
+  ],
 });
 const cors = require("cors");
 const express = require("express");
